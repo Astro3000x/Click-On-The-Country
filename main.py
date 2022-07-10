7b41e1ff2fdd64063ef297298b2b6325c3a0a2f9
Astro3000x <94796723+Astro3000x@users.noreply.github.com>
2022-07-10 14:29:51 -0700

    Created ClickCountry class.

--- main.py
+++ main.py
@@ -4,72 +4,74 @@ import random
 from shapely.geometry import Point
 
 
+class ClickCountry:
+    def __init__(self):
+        self.world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+        self.win = False
+        self.choosecountry()
+        self.showmap()
 
-world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
-win = False
-def choosecountry():
-    random_country = world.iloc[random.randint(0, len(world))]
-    return random_country
-random_countryinfo = choosecountry()
-countryname = random_countryinfo["name"]
-randomcountrygeometry = random_countryinfo["geometry"]
+    def choosecountry(self):
+        i = random.randint(0, len(self.world))
+        self.random_countryinfo = self.world.iloc[i]
+        self.country_series = self.world.iloc[i:i+1]
+        self.countryname = self.random_countryinfo["name"]
+        self.randomcountrygeometry = self.random_countryinfo["geometry"]
 
-fig, ax = plt.subplots(num=f"Double Click on {countryname}")
-ax = world.plot(ax=ax,linewidth=1 ,edgecolor="black")
-#fig.tight_layout()
-world[world.name == random_countryinfo["name"]].plot(ax=ax)
-coords = []
+    def showmap(self):
+        self.fig, self.ax = plt.subplots(num=f"Double Click on {self.countryname}")
+        self.ax = self.world.plot(ax=self.ax, linewidth=1, edgecolor="black")
+        self.updatemap()
+        self.ax.figure.canvas.mpl_connect('button_press_event', self.onclick)
 
-global eventcopy
-def whatcountry(long : float, lat : float):
-    point = Point([long, lat])
-    for i,row in world.iterrows():
-        if row["geometry"].contains(point):
-            return row["name"]
-    else:
-        return "not a country"
+    def updatemap(self):
+        self.ax.set(aspect=1, title=f"Double Click On The Country\nCountry: {self.countryname}")
 
+        # fig.tight_layout()
+        # self.random_countryinfo["name"].plot(ax=self.ax)
+        # self.world[self.world.name == self.random_countryinfo["name"]].plot(ax=self.ax)
 
-def onclick(event):
-    if not event.dblclick:
-        return
-    global win
-    global randomcountrygeometry
-    centroid = randomcountrygeometry.centroid
-    global ax
-    global eventcopy
-    eventcopy = event
-    global ix, iy
-    ix, iy = event.xdata, event.ydata
-    print(event)
-    print('x = %f, y = %f'%(ix, iy))
-    countryname = whatcountry(ix, iy)
-    global coords
-    coords.append((ix, iy))
-    point = Point(ix, iy)
-    if randomcountrygeometry.contains(point):
-        print("Victory")
+    def whatcountry(self, long: float, lat: float):
+        point = Point([long, lat])
+        for i, row in self.world.iterrows():
+            if row["geometry"].contains(point):
+                return row["name"]
+        else:
+            return "not a country"
 
-        world[world.name == random_countryinfo["name"]].plot(color="red", ax=ax)
-        ax.set(aspect=1, title=f"You Win! Country: {random_countryinfo['name']}")
-        #random_countryinfo.plot(color="red", ax=ax)
-        #ax = world.plot(ax=ax)
-        plt.pause(0.1)
+    def onclick(self, event):
+        if not event.dblclick:
+            return
+        ix, iy = event.xdata, event.ydata
+        print(event)
+        print('x = %f, y = %f' % (ix, iy))
+        countryname = self.whatcountry(ix, iy)
+        point = Point(ix, iy)
+        if self.randomcountrygeometry.contains(point):
+            print("Victory")
 
-        #plt.show(block=True)
-        win = True
-    elif win == True:
-        pass
-    else:
-        distance = randomcountrygeometry.distance(point)*111
-        ax.set(aspect=1, title=f"Country: {random_countryinfo['name']}\nNo, that's {countryname}.\nYou are {distance:.0f}km away.")
+            self.country_series.plot(color="red", ax=self.ax)
+            self.ax.set(aspect=1, title=f"You Win! Country: {self.countryname}")
+            # random_countryinfo.plot(color="red", ax=ax)
+            # ax = world.plot(ax=ax)
+            plt.pause(0.1)
 
+            # plt.show(block=True)
+            self.win = True
+        elif self.win:
+            pass
+        else:
+            distance = self.randomcountrygeometry.distance(point) * 111
+            self.ax.set(aspect=1,
+                        title=f"Country: {self.countryname}\nNo, that's {countryname}.\nYou are {distance:.0f}km away.")
 
-ax.figure.canvas.mpl_connect('button_press_event', onclick)
-ax.set(aspect=1, title=f"Double Click On The Country\nCountry: {random_countryinfo['name']}")
+def main():
+    game = ClickCountry()
+    game2 = ClickCountry()
+    plt.ion()
+    plt.show(block=True)
+    pass
 
 
-
-
-plt.ion()
-plt.show(block=True)
+if __name__ == '__main__':
+    main()
